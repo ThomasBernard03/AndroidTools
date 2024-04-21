02164082caea84ff190d435ecc4f93354c8d36bf
Thomas Bernard <thomas.bernard.310@gmail.com>
2024-04-21 17:18:39 +0200

    Add phone animation in installer view

--- AndroidTools/AndroidTools/Views/Installer/InstallerView.swift
+++ AndroidTools/AndroidTools/Views/Installer/InstallerView.swift
@@ -14,6 +14,7 @@ struct InstallerView: View {
     let deviceId : String
     
     @State private var dropTargetted: Bool = false
+    @State private var isHoveringPhone : Bool = false
     @ObservedObject private var viewModel = InstallerViewModel()
     
     
@@ -37,69 +38,66 @@ struct InstallerView: View {
         
         Text("Easily install an application on the connected phone using an .apk file")
         
-        
-        
-        Image("AndroidApk")
-            .resizable()
-            .cornerRadius(44)
-            .frame(width: 200, height: 200)
-        
-            .onDrop(of: [.apk], isTargeted: $dropTargetted) { providers in
-                providers.first?.loadItem(forTypeIdentifier: UTType.apk.identifier, options: nil) { (item, error) in
-                    
-                    if let item = item as? URL {
-                        let fileUrl = item.startAccessingSecurityScopedResource() ? item : URL(fileURLWithPath: item.path)
-                        viewModel.installApk(deviceId: deviceId, path: fileUrl.path)
-                        item.stopAccessingSecurityScopedResource()
-                    }
-                }
-                
-                
-                return true
-            }
-            .overlay {
-                if dropTargetted {
-                    ZStack {
-                        Color.black.opacity(0.5)
+        ZStack {
+            
+            Image("AndroidApk")
+                .resizable()
+                .cornerRadius(44)
+                .frame(width: 200, height: 200)
+            
+                .onDrop(of: [.apk], isTargeted: $dropTargetted) { providers in
+                    providers.first?.loadItem(forTypeIdentifier: UTType.apk.identifier, options: nil) { (item, error) in
                         
-                        VStack(spacing: 8) {
-                            Image(systemName: "plus.circle.fill")
-                                .font(.system(size: 30))
-                            Text("Drop your apk here...")
+                        if let item = item as? URL {
+                            let fileUrl = item.startAccessingSecurityScopedResource() ? item : URL(fileURLWithPath: item.path)
+                            viewModel.installApk(deviceId: deviceId, path: fileUrl.path)
+                            item.stopAccessingSecurityScopedResource()
                         }
-                        .font(.title2)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: 250)
-                        .multilineTextAlignment(.center)
                     }
-                    .cornerRadius(44)
+                    
+                    
+                    return true
                 }
-                else {
-                    Text("Click or drag .apk")
-                        .foregroundStyle(.white)
-                        .offset(CGSize(width: 0, height: 60.0))
+                .overlay {
+                    if dropTargetted {
+                        ZStack {
+                            Color.black.opacity(0.5)
+                            
+                            VStack(spacing: 8) {
+                                Image(systemName: "plus.circle.fill")
+                                    .font(.system(size: 30))
+                                Text("Drop your apk here...")
+                            }
+                            .font(.title2)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: 250)
+                            .multilineTextAlignment(.center)
+                        }
+                        .cornerRadius(44)
+                    }
+                    else {
+                        Text("Click or drag .apk")
+                            .foregroundStyle(.white)
+                            .offset(CGSize(width: 0, height: 60.0))
+                    }
                 }
-            }
-            .animation(.default, value: dropTargetted)
-            .padding()
-            .onTapGesture {
-                loadApkFile()
-            }
-        
-        
-        switch viewModel.installStatus {
-        case .notStarted : Text("Not started")
-        case .loading(let fileName) : ProgressView(){
-            Text("Installing \(fileName)")
-        }
-        .progressViewStyle(.linear)
-        .padding()
-        case .success : Text("Apk installed")
-        case .error(let message) : Text("Error \(message)")
-                .foregroundStyle(.red)
+                .animation(.default, value: dropTargetted)
+                .padding()
+                .onTapGesture {
+                    loadApkFile()
+                }
+
+            Image("HomeScreenPhone")
+                .resizable()
+                .scaledToFit()
+                .onHover { hovering in
+                    isHoveringPhone = hovering
+                }
+                .offset(y: isHoveringPhone ? 180 : 200)
+                .animation(.easeInOut(duration: 0.2), value: isHoveringPhone)
+                .frame(maxWidth: 300)
         }
     }
-        
 }
 
 #Preview {
