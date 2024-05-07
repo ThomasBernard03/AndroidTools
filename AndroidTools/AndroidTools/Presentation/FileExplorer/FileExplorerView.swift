007d3f8533a6c261a975af30caa1839d7bf17165
Thomas Bernard <thomas.bernard.310@gmail.com>
2024-05-07 15:14:01 +0200

    Use external view for table

--- AndroidTools/AndroidTools/Presentation/FileExplorer/FileExplorerView.swift
+++ AndroidTools/AndroidTools/Presentation/FileExplorer/FileExplorerView.swift
@@ -26,14 +26,6 @@ struct FilesView: View {
     @State private var viewModel = FilesViewModel()
     @State private var selection: IdentifiableFileExplorerItem.ID? = nil
     
-    private var itemDateFormatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .short
-        formatter.timeStyle = .short
-        return formatter
-    }
-
-    
     var body: some View {
         
         let items = (viewModel.fileExplorerResult?.childrens ?? []).map { item in
@@ -43,61 +35,12 @@ struct FilesView: View {
         var currentItem: FileExplorerItem? {
             return items.first { $0.id == selection }?.item
         }
-
-
         
-        Table(of: IdentifiableFileExplorerItem.self, selection: $selection){
-            TableColumn("Name") { wrapper in
-                if let fileItem = wrapper.item as? FileItem {
-                    FileRow(name: fileItem.name)
+        ZStack {
+            if let safeFileExplorerResult = viewModel.fileExplorerResult {
+                TableView(selection: $selection, items: items, fileExplorerResult: safeFileExplorerResult) { path in
+                    viewModel.getFiles(deviceId: deviceId, path: path)
                 }
-                else {
-                    Text(wrapper.item.name)
-                }
-            }
-            
-            TableColumn("Last Modified") { wrapper in
-                if let fileItem = wrapper.item as? FileItem {
-                    Text("\(fileItem.lastModificationDate, formatter: itemDateFormatter)")
-                    // Text(fileItem.lastModificationDate, style: .date)
-                } else {
-                    EmptyView()
-                }
-            }
-            .width(140)
-            
-            TableColumn("Size"){ wrapper in
-                if let fileItem = wrapper.item as? FileItem {
-                    Text(fileItem.size.toSize())
-                }
-                else {
-                    EmptyView()
-                }
-            }
-            .width(80)
-        } rows: {
-            ForEach(items, id: \.id){ wrapper in
-                TableRow(wrapper)
-                    .contextMenu {
-                        Button("Rename") {
-                            // TODO open editor in inspector
-                        }
-                        Divider()
-                        Button("Delete", role: .destructive) {
-                            
-                        }
-                    }
-                    
-            }
-        }
-        .contextMenu(forSelectionType: String.self, menu: { _ in }) {_ in
-            let currentItem = currentItem?.name
-            if viewModel.fileExplorerResult?.fullPath.isEmpty ?? false {
-                viewModel.getFiles(deviceId: deviceId, path: currentItem ?? "")
-            }
-            else {
-                let path = "\(viewModel.fileExplorerResult?.fullPath ?? "")/\(currentItem ?? "")"
-                viewModel.getFiles(deviceId: deviceId, path: path)
             }
 
         }
