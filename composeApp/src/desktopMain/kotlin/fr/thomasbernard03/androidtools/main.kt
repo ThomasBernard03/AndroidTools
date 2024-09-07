2e89eb328f3a7f1c8bf9b8d6cc194c41d5529f5f
Thomas Bernard <thomas.bernard@ikmail.com>
2024-09-07 21:19:25 +0200

    Can install app with drag and drop .apk

--- composeApp/src/desktopMain/kotlin/fr/thomasbernard03/androidtools/main.kt
+++ composeApp/src/desktopMain/kotlin/fr/thomasbernard03/androidtools/main.kt
@@ -1,16 +1,10 @@
 package fr.thomasbernard03.androidtools
 
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowSize
-import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import fr.thomasbernard03.androidtools.presentation.information.InformationViewModel
 import fr.thomasbernard03.androidtools.presentation.main.MainScreen
 import fr.thomasbernard03.androidtools.presentation.main.MainViewModel
 import fr.thomasbernard03.androidtools.presentation.theme.AndroidToolsTheme
--- composeApp/src/desktopMain/kotlin/fr/thomasbernard03/androidtools/presentation/applicationinstaller/ApplicationInstallerScreen.kt
+++ composeApp/src/desktopMain/kotlin/fr/thomasbernard03/androidtools/presentation/applicationinstaller/ApplicationInstallerScreen.kt
@@ -3,25 +3,67 @@ package fr.thomasbernard03.androidtools.presentation.applicationinstaller
 import androidtools.composeapp.generated.resources.Res
 import androidtools.composeapp.generated.resources.folder
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.onDrag
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.DragData
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.ComposeWindow
+import androidx.compose.ui.onExternalDrag
 import fr.thomasbernard03.androidtools.presentation.applicationinstaller.components.WavesAnimation
 import org.jetbrains.compose.resources.painterResource
 import java.awt.FileDialog
+import java.awt.datatransfer.DataFlavor
+import java.awt.dnd.DnDConstants
+import java.awt.dnd.DropTarget
+import java.awt.dnd.DropTargetAdapter
+import java.awt.dnd.DropTargetDropEvent
+import java.io.File
 import java.io.FilenameFilter
+import javax.swing.JFrame
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
 fun ApplicationInstallerScreen(uiState : ApplicationInstallerUiState, onEvent: (ApplicationInstallerEvent) -> Unit) {
 
+    var hoverred by remember { mutableStateOf(false) }
+
     Scaffold(
+        modifier = Modifier
+            .onExternalDrag(
+                enabled = !uiState.loading,
+                onDragStart = {
+                    hoverred = true
+                },
+                onDragExit = {
+                    hoverred = false
+                },
+                onDrop = { externalDragValue ->
+                    hoverred = false
+                    if (externalDragValue.dragData is DragData.FilesList) {
+                        val draggedFiles = (externalDragValue.dragData as DragData.FilesList).readFiles().map { it.drop(5) } // Remove file: prefix
+                        draggedFiles.firstOrNull()?.let {
+                            onEvent(ApplicationInstallerEvent.OnInstallApplication(it))
+                        }
+                    }
+                }
+            ),
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
@@ -44,10 +86,15 @@ fun ApplicationInstallerScreen(uiState : ApplicationInstallerUiState, onEvent: (
             }
         }
     ) {
-
-
         Box(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .then(
+                    if (hoverred)
+                        Modifier.background(MaterialTheme.colorScheme.onBackground.copy(0.2f))
+                    else
+                        Modifier
+                )
         ) {
             WavesAnimation()
 
@@ -59,6 +106,7 @@ fun ApplicationInstallerScreen(uiState : ApplicationInstallerUiState, onEvent: (
                     modifier = Modifier.fillMaxWidth()
                 )
             }
+
         }
     }
 }
