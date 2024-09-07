50dabdc5fe73d5b2beff4c90c4adb05dc18fd781
Thomas Bernard <thomas.bernard@ikmail.com>
2024-09-07 14:31:22 +0200

    Add navigation

--- composeApp/src/desktopMain/kotlin/fr/thomasbernard03/androidtools/presentation/main/MainScreen.kt
+++ composeApp/src/desktopMain/kotlin/fr/thomasbernard03/androidtools/presentation/main/MainScreen.kt
@@ -2,6 +2,7 @@ package fr.thomasbernard03.androidtools.presentation.main
 
 import androidtools.composeapp.generated.resources.Res
 import androidtools.composeapp.generated.resources.app_installer
+import androidtools.composeapp.generated.resources.information
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.NavigationRail
@@ -11,6 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import fr.thomasbernard03.androidtools.presentation.applicationinstaller.ApplicationInstallerScreen
 import fr.thomasbernard03.androidtools.presentation.applicationinstaller.ApplicationInstallerViewModel
 import org.jetbrains.compose.resources.painterResource
@@ -19,11 +25,13 @@ import org.jetbrains.compose.resources.painterResource
 @Composable
 fun MainScreen() {
     Row {
-        NavigationRail(
-        ){
+        val navController: NavHostController = rememberNavController()
+        val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+
+        NavigationRail {
             NavigationRailItem(
-                selected = true,
-                onClick = {},
+                selected = currentRoute == "appInstaller",
+                onClick = { navController.navigate("appInstaller") },
                 icon = {
                     Image(
                         painter = painterResource(Res.drawable.app_installer),
@@ -32,11 +40,30 @@ fun MainScreen() {
                 },
                 label = { Text("App Installer") }
             )
+
+            NavigationRailItem(
+                selected = currentRoute == "information",
+                onClick = { navController.navigate("information") },
+                icon = {
+                    Image(
+                        painter = painterResource(Res.drawable.information),
+                        contentDescription = "Information"
+                    )
+                },
+                label = { Text("App Installer") }
+            )
         }
 
-        val viewModel = viewModel { ApplicationInstallerViewModel() }
-        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-        ApplicationInstallerScreen(uiState = uiState, onEvent = viewModel::onEvent)
+        NavHost(navController = navController, startDestination = "appInstaller") {
+            composable("appInstaller") {
+                val viewModel = viewModel { ApplicationInstallerViewModel() }
+                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+                ApplicationInstallerScreen(uiState = uiState, onEvent = viewModel::onEvent)
+            }
+            composable("information") {
+                Text("Hello from information")
+            }
+        }
     }
 
 }
