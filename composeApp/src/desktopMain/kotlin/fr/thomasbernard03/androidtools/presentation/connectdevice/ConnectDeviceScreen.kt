3b8fa4e658bdc4468c6aba6640dad1832937b3f7
Thomas Bernard <thomas.bernard@ikmail.com>
2024-09-26 19:30:07 +0200

    Improve connect to device screen

--- composeApp/src/desktopMain/kotlin/fr/thomasbernard03/androidtools/presentation/connectdevice/ConnectDeviceScreen.kt
+++ composeApp/src/desktopMain/kotlin/fr/thomasbernard03/androidtools/presentation/connectdevice/ConnectDeviceScreen.kt
@@ -1,9 +1,21 @@
 package fr.thomasbernard03.androidtools.presentation.connectdevice
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun ConnectDeviceScreen() {
-    Text("No device connected, please connect one and retry")
+    Scaffold {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "No device connected, please connect one and retry",
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
 }
