ebc624042995410876bd88cb29632202399c0640
Thomas Bernard <thomas.bernard@ikmail.com>
2026-01-06 22:34:01 +0100

    chore(information): add device battery design

--- lib/features/information/presentation/information_screen.dart
+++ lib/features/information/presentation/information_screen.dart
@@ -1,5 +1,4 @@
 import 'package:android_tools/features/information/presentation/information_bloc.dart';
-import 'package:android_tools/features/information/presentation/widgets/android_version_card.dart';
 import 'package:android_tools/features/information/presentation/widgets/apk_installer_drop_target.dart';
 import 'package:android_tools/features/information/presentation/widgets/information_recap_item.dart';
 import 'package:android_tools/shared/presentation/refresh_device_button.dart';
@@ -45,43 +44,80 @@ class InformationScreen extends StatelessWidget {
                       ),
                       Row(
                         spacing: 16,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Image.asset("assets/pixel_10.png", width: 200),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            spacing: 8,
-                            children: [
-                              InformationRecapItem(
-                                label: "Manufacturer",
-                                value:
-                                    state.deviceInformation?.manufacturer ??
-                                    "-",
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.white.withAlpha(25),
+                                  blurRadius: 64,
+                                  offset: const Offset(0, 10),
+                                ),
+                              ],
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(16),
+                              child: Image.asset(
+                                "assets/pixel_10.png",
+                                height: 400,
                               ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 400,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              spacing: 8,
+                              children: [
+                                InformationRecapItem(
+                                  label: "Manufacturer",
+                                  value:
+                                      state.deviceInformation?.manufacturer ??
+                                      "-",
+                                ),
 
-                              InformationRecapItem(
-                                label: "Serial Number",
-                                value:
-                                    state.deviceInformation?.serialNumber ??
-                                    "-",
-                              ),
-                              InformationRecapItem(
-                                label: "Model",
-                                value: state.deviceInformation?.model ?? "-",
-                              ),
-                            ],
+                                InformationRecapItem(
+                                  label: "Serial Number",
+                                  value:
+                                      state.deviceInformation?.serialNumber ??
+                                      "-",
+                                ),
+                                InformationRecapItem(
+                                  label: "Model",
+                                  value: state.deviceInformation?.model ?? "-",
+                                ),
+                                InformationRecapItem(
+                                  label: "Android Version",
+                                  value:
+                                      state.deviceInformation?.version ?? "-",
+                                ),
+
+                                const Spacer(),
+
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Text("100%"),
+                                    SizedBox(
+                                      width: 60,
+                                      height: 4,
+                                      child: LinearProgressIndicator(
+                                        color: Colors.white,
+                                        backgroundColor: Color(0xFF2B2F33),
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
                       Wrap(
                         children: [
-                          BlocBuilder<InformationBloc, InformationState>(
-                            builder: (context, state) {
-                              return AndroidVersionCard(
-                                androidVersion:
-                                    state.deviceInformation?.version ?? "",
-                              );
-                            },
-                          ),
                           ApkInstallerDropTarget(
                             onInstallApk: (path) {
                               context.read<InformationBloc>().add(
